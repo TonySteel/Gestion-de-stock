b73c51f26eef13822f433ec1672ea787fc943f61
Tony Steel <60441474+TonySteel@users.noreply.github.com>
2020-11-14 15:05:06 +0100

    Add files via upload

---
+++ SQLClass/Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SQLClass
+{
+    public class SQLClass
+    {
+        private string server;
+        private string database;
+        private string user;
+        private string password;
+
+        public SQLClass(string server, string database, string user, string password)
+        {
+            
+        }
+    }
+}
